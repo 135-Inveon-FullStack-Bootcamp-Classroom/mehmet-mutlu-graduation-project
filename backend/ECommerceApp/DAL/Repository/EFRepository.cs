40e9ee68b3dd6c4cb9b75c36703394ff877060bb
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-12-08 00:35:35 +0300

    Init: Backend .Net Core project and Data Layer was created

---
+++ backend/ECommerceApp/DAL/Repository/EFRepository.cs
@@ -0,0 +1,65 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly ApplicationEFContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public EFRepository(ApplicationEFContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+        public void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Delete(int Id)
+        {
+            var entity = GetById(Id);
+            Delete(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            if (entity == null)
+            {
+                return;
+            }
+            _dbSet.Remove(entity);
+        }
+
+        public IQueryable<TEntity> Get(Expression<Func<TEntity, bool>> predicate = null)
+        {
+            if (predicate != null)
+            {
+                return _dbSet.Where(predicate);
+            }
+            return _dbSet;
+        }
+
+        public List<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public TEntity GetById(int Id)
+        {
+            return _dbSet.Find(Id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
---
+++ backend/ECommerceApp/Entities/ProductEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Table("Products")]
+    public class ProductEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int StockAmount { get; set; }
+        public int SoldAmount { get; set; }
+        public List<string> ImageURLs { get; set; }
+        public int CategoryId { get; set; }
+        public virtual CategoryEntity Category { get; set; }
+    }
+}

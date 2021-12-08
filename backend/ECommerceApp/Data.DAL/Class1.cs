6436e93a87b7a752109b9df5433b9b5a80d75663
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-12-09 00:14:10 +0300

    Ref: Entities and DAL were edited

---
+++ backend/ECommerceApp/Data.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Data.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ backend/ECommerceApp/ECommerceApp.Data.Entities/CategoryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Data.Entities
+{
+    [Table("Categories")]
+    public class CategoryEntity : BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual ICollection<ProductEntity> Products { get; set; }
+    }
+}

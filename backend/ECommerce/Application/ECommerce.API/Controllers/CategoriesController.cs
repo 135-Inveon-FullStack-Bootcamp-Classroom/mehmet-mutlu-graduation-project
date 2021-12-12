65a1664a676de5d69f56d77c116e2f500f1afcb4
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-12-12 15:27:05 +0300

    Feat: Web API was created in Application folder and CatagoriesController was created

---
+++ backend/ECommerce/Application/ECommerce.API/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Business.Engines.Interfaces;
+using ECommerce.Business.Engines.Models.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.API.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        private readonly ICatalogEngine _catalogEngine;
+        public CategoriesController(ICatalogEngine catalogEngine)
+        {
+            _catalogEngine = catalogEngine;
+        }
+
+        [HttpGet("{id}/products")]
+        public List<ProductViewModel> GetCategoryProducts(int id)
+        {
+            var data = _catalogEngine.GetProductListByCategoryIdForCatalog(id);
+            return data;
+        }
+    }
+}

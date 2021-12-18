89f2fc63bc04a9d946f3d32a938914872f2b287b
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-12-18 16:43:42 +0300

    Feat-UI: Layout, App and index were edited for new components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import Layout from './layout/Layout';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Slider from "./components/Slider/Slider";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <Layout>
-      Frontend Project
+      <Navbar />
+      <Slider />
     </Layout>
   );
 }

77a99cd1e5cc3671a04c21881b61389c759914f8
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-12-29 00:30:00 +0300

    UI: CategoryProducts' sidebar was designed

--- frontend/src/pages/CategoryProducts/CategoryProducts.tsx
+++ frontend/src/pages/CategoryProducts/CategoryProducts.tsx
@@ -32,16 +32,32 @@ const CategoryProducts = () => {
   return (
     <div className="category-products">
       <div className="category-products-left">
-        <h3 className="title">Ürünleri Filtrele</h3>
-        <div className="sorting">
-          <select name="" id="">
-            <option disabled selected>Sıralama tipi seçiniz</option>
-            <option value="1">İsme göre artan</option>
-            <option value="2">İsme göre azalan</option>
-            <option value="3">Fiyata göre artan</option>
-            <option value="4">Fiyata göre azalan</option>
-          </select>
-        </div>
+        <form action="">
+          <h3 className="title">Ürünleri Filtrele</h3>
+          <div className="sorting">
+            <p className="sub-title">Sıralama Tipi</p>
+            <select name="" id="">
+              <option disabled selected>
+                Sıralama tipi seçiniz
+              </option>
+              <option value="1">İsme göre artan</option>
+              <option value="2">İsme göre azalan</option>
+              <option value="3">Fiyata göre artan</option>
+              <option value="4">Fiyata göre azalan</option>
+            </select>
+          </div>
+          <div className="price-range">
+            <p className="sub-title">Fiyat Aralığı</p>
+            <input type="number" />
+            -
+            <input type="number" />
+          </div>
+          <div className="search-name">
+            <p className="sub-title">Ürün Adı</p>
+            <input type="search" />
+          </div>
+          <button type="submit" className="submit-button">Arama Yap</button>
+        </form>
       </div>
       <div className="category-products-right">
         {items.map((item) => (
